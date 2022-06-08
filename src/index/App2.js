c1affc7a9c00bc61dad160b726e465b95584b0b0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-08 16:31:07 -0500

    completed index page

---
+++ src/index/App2.js
@@ -0,0 +1,12 @@
+import {Report} from "./Report";
+
+import './App2.css';
+import {useState} from "react";
+import {ControlledModal} from "./ControlledModal";
+
+export default function App2() {
+
+    return (<Report>
+        </Report>
+    )
+}
---
+++ src/index/Report.js
@@ -0,0 +1,23 @@
+import {ControlledModal} from "./ControlledModal";
+import {useState} from "react";
+
+export const Report = () => {
+
+    const [shouldShowModal, setShouldShowModal] = useState(false);
+    return (
+        <section className="index">
+            <h1 className="index--h1">Do you want to make a rapid report?</h1>
+
+            <ControlledModal
+                shouldShow={shouldShowModal}
+                onClose={() => setShouldShowModal(false)}
+            />
+            <div className="container">
+                <button className="yes"
+                        onClick={() => setShouldShowModal(!shouldShowModal)}>Yes</button>
+
+                <button className="no" onClick={() => console.log("no")}>No</button>
+            </div>
+        </section>
+    )
+}
