fec2c4a512734c7240c601e78134b7158bbc598d
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-09 15:32:15 -0500

    adding tailwind config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-      "./src/coordinatesForm/FormWithGeoLocation/*.{js}"
+    "./src/***/*.{js,jsx,ts,tsx}",
+    "./src/*.js"
   ],
   theme: {
     extend: {},
