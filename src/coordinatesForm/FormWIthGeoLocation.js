8f24acbb9e42f9103d8ebce4e5128cde9f3eace8
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-09 08:53:47 -0500

    grabbing values from text area

--- src/coordinatesForm/FormWIthGeoLocation.js
+++ src/coordinatesForm/FormWIthGeoLocation.js
@@ -1,30 +1,56 @@
 import './form.css'
+import React, {useState} from "react";
+
+const initialFormData = Object.freeze({
+    select_category: "",
+    select_size: "",
+    extra_details: ""
+});
 
 export const FormWIthGeoLocation = (props) => {
+    const [formData, updateFormData] = useState(initialFormData);
+
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+
+            // Trimming any whitespace
+            [e.target.name]: e.target.value.trim()
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData);
+        // ... submit to API or something
+    };
     return (
         <form>
-            <label htmlFor="select-category">Select category</label>
-            <select name="selectList" id="select-category">
-                <option value="landmine">landmine</option>
-                <option value="grenade">grenade</option>
-            </select>
             <label htmlFor="lat"> your latitude</label>
             <input type="text" id="lat" onChange={() => props.lat ? props.lat : ""} value={props.lat ? props.lat : ""}/>
 
             <label htmlFor="lon"> your longitude</label>
             <input type="text" id="lon" onChange={() => props.lon ? props.lon : ""} value={props.lon ? props.lon : ""}/>
 
+            <label htmlFor="select_category">Select category</label>
+            <select id="select_category" name="select_category" onChange={handleChange}>
+                <option value="">N/A</option>
+                <option value="landmine">landmine</option>
+                <option value="grenade">grenade</option>
+            </select>
 
-            <label htmlFor="select-size">select size</label>
-            <select name="select-size" id="select-size">
+            <label htmlFor="select_size">select size</label>
+            <select name="select_size" id="select_size" onChange={handleChange}>
                 <option value="S">small</option>
                 <option value="M">medium</option>
                 <option value="L">large</option>
                 <option value="XL">extra large</option>
             </select>
-            <label htmlFor="text-area">Extra details: </label>
+
+            <label htmlFor="text-area" onChange={handleChange} id="extra_details">Extra details: </label>
 
             <textarea id="text-area" name="text-area" rows="4" cols="50"/>
+            <button onClick={handleSubmit}>Submit</button>
         </form>
     )
 }
