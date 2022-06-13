8334c3ecec7fccdbc4596b71d23ecc99e1f962a6
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-13 15:09:05 -0500

    deleted files

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const S3_BUCKET = 'oceans2';
-export const REGION = 'us-east-2';
-export const ACCESS_KEY = 'AKIA5BUWQB4PNAY55UXG';
-export const SECRET_ACCESS_KEY = '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';
