18094f9371006d2c6cf978a1a0c8f4492fe6f262
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-06-14 17:26:16 -0400

    add working router pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import {Report} from "./index/Report";
-
+import {Routes, Route, Link } from "react-router-dom";
+import {Home} from "./Home.js";
+import {About} from "./About.js";
 
 export default function App() {
-
     return (
-        <div className="report-container">
-            <Report>
-            </Report>
+        <div className="App">
+            <h1>Welcome to React Router!</h1>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="report" element={<Report />} />
+            </Routes>
         </div>
     )
 }
+
+
