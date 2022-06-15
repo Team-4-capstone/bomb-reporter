13e33db19b19d0a21dfc5d0b832e6868f83f15e4
George Puryear <gm.puryear@yahoo.com>
2022-06-14 23:03:03 -0500

    diabled button if pictures not uploaded first

--- src/Report.js
+++ src/Report.js
@@ -6,20 +6,20 @@ import './index/App.css';
 import Geolocator from "./coordinatesForm/Geolocator";
 
 export function Report() {
-    const [shouldShowModal, setShouldShowModal] = useState(false);
+    // const [shouldShowModal, setShouldShowModal] = useState(false);
     return (
         <>
             <main>
                 <Geolocator/>
                 <h2><button>Submit</button> current location/ Report Rapid UXO</h2>
-                <ControlledModal
-                    shouldShow={shouldShowModal}
-                    onClose={() => setShouldShowModal(false)}
-                />
-                <button className=""
-                        onClick={() => setShouldShowModal(!shouldShowModal)}>
-                    <span className="btnText">Add More Details</span>
-                </button>
+                {/*<ControlledModal*/}
+                {/*    shouldShow={shouldShowModal}*/}
+                {/*    onClose={() => setShouldShowModal(false)}*/}
+                {/*/>*/}
+                {/*<button className=""*/}
+                {/*        onClick={() => setShouldShowModal(!shouldShowModal)}>*/}
+                {/*    <span className="btnText">Add More Details</span>*/}
+                {/*</button>*/}
 
                 <h2>MapBOX</h2>
 
--- src/coordinatesForm/Geolocator.js
+++ src/coordinatesForm/Geolocator.js
@@ -2,12 +2,14 @@ import React, {Component, useEffect} from "react";
 import {useState} from "react";
 import './form.css'
 import {SelectDropdownMenu} from "./SelectDropdownMenu";
+import {ControlledModal} from "../index/ControlledModal";
+
 
 export default function Geolocator() {
     const [lat, setLat] = useState(null);
     const [lng, setLng] = useState(null);
     const [status, setStatus] = useState(null)
-
+    const [shouldShowModal, setShouldShowModal] = useState(false);
     useEffect(() => {
         // storing input name
         localStorage.setItem("lat", JSON.stringify(lat));
@@ -32,6 +34,15 @@ export default function Geolocator() {
     return (
         <>
             <button className="get-location" onClick={getLocation}>Get Location</button>
+            <ControlledModal
+                shouldShow={shouldShowModal}
+                onClose={() => setShouldShowModal(false)}
+            />
+
+            {lat ? <button className="" disabled={false}
+                    onClick={() => setShouldShowModal(!shouldShowModal)}>
+                <span className="btnText">Add More Details</span>
+            </button> : ""}
         </>
     )
 }
