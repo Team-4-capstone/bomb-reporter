7c26615436da0194024223e588a13ec50e06017d
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-06-20 13:40:41 -0400

    add button fix

--- src/coordinatesForm/Geolocator.js
+++ src/coordinatesForm/Geolocator.js
@@ -72,7 +72,7 @@ export default function Geolocator() {
             <MapBox lat={lat} lng={lng}/>
         </>
     ) :
-        <button className="inline-block h-72 w-72 rounded-full text-gray-700 bg-gradient-to-r from-yellow-400 to-blue-600
+        <button className="inline-block h-72 w-72 rounded-full text-gray-700  border-solid bg-gradient-to-r from-yellow-400 to-blue-600
                 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium
     text-2xl px-5 py-2.5 text-center mr-2 mb-2" onClick={getLocation}>Submit Location</button>
 
