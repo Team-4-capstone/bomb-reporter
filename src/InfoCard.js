c27eca9ef9dae40c9ae63b529242d386c4e2a0cd
George Puryear <gm.puryear@yahoo.com>
2022-06-21 14:03:46 -0500

    added cards

--- src/InfoCard.js
+++ src/InfoCard.js
@@ -25,26 +25,45 @@ function InfoCard() {
 
     return (
         reports.map((report) => (
-            <Card key={report.id} className="rounded overflow-hidden shadow-lg"
-                  imgAlt="Meaningful alt text for an image that is not purely decorative" style={{width:314}}
-                  imgSrc={report.description.img_path}>
-                <div className="w-150">
-                    <ListGroup>
-                        <ListGroup.Item>
-                            Category: {report.category.category}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <p>Location: Latitude: {report.location.latitude} Longitude: {report.location.longitude}</p>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Messages
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Download
-                        </ListGroup.Item>
-                    </ListGroup>
+            <div key={report.id} className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <img className="rounded-t-lg w-40" src="{report.description.img_path}" alt=""/>
+                <div className="p-5">
+                    <p>Category: {report.category.category}</p>
+                    <p>Location: Latitude: {report.location.latitude} Longitude: {report.location.longitude}</p>
+                    <a href="#"
+                       className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        More Details
+                        <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                  clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
                 </div>
-            </Card>)))
+            </div>)))
+
+
+            // <Card key={report.id} className="rounded overflow-hidden shadow-lg"
+            //       imgAlt="Meaningful alt text for an image that is not purely decorative" style={{width:314}}
+            //       imgSrc={report.description.img_path}>
+            //     <div className="w-150">
+            //         <ListGroup>
+            //             <ListGroup.Item>
+            //                 Category: {report.category.category}
+            //             </ListGroup.Item>
+            //             <ListGroup.Item>
+            //                 <p>Location: Latitude: {report.location.latitude} Longitude: {report.location.longitude}</p>
+            //             </ListGroup.Item>
+            //             <ListGroup.Item>
+            //                 Messages
+            //             </ListGroup.Item>
+            //             <ListGroup.Item>
+            //                 Download
+            //             </ListGroup.Item>
+            //         </ListGroup>
+            //     </div>
+            // </Card>)))
 }
 
 export default InfoCard;
