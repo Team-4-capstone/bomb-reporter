f496bcfd8f25d36e3ffbd4e579142e4a94334002
George Puryear <gm.puryear@yahoo.com>
2022-06-24 11:35:53 -0500

    added style to button

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ import Geolocator from "./coordinatesForm/Geolocator";
 export function Home() {
     return (
         <>
-            <main className="index flex h-full justify-center mt-10">
+            <main className="index flex h-full justify-center">
                     <Geolocator/>
             </main>
             <div className="fill-window flex justify-center">
