82d6f21975a47c86635c00d3eaf4d90a80de8287
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-06-30 15:24:19 -0400

    button was askew in report only

--- src/pages/Report.js
+++ src/pages/Report.js
@@ -18,8 +18,8 @@ export function Report() {
             <div className="fill-window flex justify-center">
                 <nav className="fixed bottom-10">
                     <button className="h-20 w-20 rounded-full border-solid border-solid bg-gradient-to-r from-yellow-400 to-blue-600 text-gray-600 font-bold">HOME
-                        <IconContext.Provider value={{ color: "#5A5560", className: "global-class-name", size: "2em"}}>
-                            <Link to="/connection">
+                        <IconContext.Provider value={{color: "#5A5560", size: "2.2em"}}>
+                            <Link className="animate-bounce" to="/connection">
                                 <HiChevronDoubleDown/>
                             </Link>
                         </IconContext.Provider>
